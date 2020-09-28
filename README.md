64195c16ebe60f7fd0132386daea2d0e3930b5ea
Bartosz <WonderfulLifeVision@gmail.com>
2020-09-28 13:31:05 +0200

    adding course with toast

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 
 
 
-In the project directory, you can run:
-### `npm start`
+### In the project directory, you can run:
+ `npm start`
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
+import { toast } from 'react-toastify';
 
 const ManageCoursePage = props => {
   const [course, setCourse] = useState({
@@ -10,15 +12,22 @@ const ManageCoursePage = props => {
     category: ""
   })
 
-  function handleTitleChange(event) {
-    const updatedCourse = { ...course, title: event.target.value };
-    setCourse(updatedCourse);
+  function handleChange({ target }) {
+    setCourse({ ...course, [target.name]: target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success('Course saved.');
+    })
   }
 
   return (
     <>
       <h2>Manage Course</h2>
-      <CourseForm course={course} onTitleChange={handleTitleChange} />
+      <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} />
     </>
   );
 };
